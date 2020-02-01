7baa3e56b9e02bbd5735b31f2f748c4956e7e194
TSTD12738 TEMP-STUDENT <tstd12738@live.uwe.ac.uk>
2020-02-01 15:01:35 +0000

    We can now switch body parts

---
+++ GGJ2020Axolotl/Assets/Scripts/BodyParts/InitialiseBodyParts.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitialiseBodyParts : MonoBehaviour
+{
+    [SerializeField] private Body _axo;
+    [SerializeField] private Body _lotl;
+
+    [SerializeField] private BodyPart _arm1;
+    [SerializeField] private BodyPart _arm2;
+    [SerializeField] private BodyPart _arm3;
+    [SerializeField] private BodyPart _arm4;
+
+    [SerializeField] private BodyPart _leg1;
+    [SerializeField] private BodyPart _leg2;
+    [SerializeField] private BodyPart _leg3;
+
+    private void Awake()
+    {
+        _axo.Recieve(_arm1);
+        _axo.Recieve(_arm2);
+        _lotl.Recieve(_arm3);
+        _axo.Recieve(_arm4);
+
+        _axo.Recieve(_leg1);
+        _lotl.Recieve(_leg2);
+        _lotl.Recieve(_leg3);
+
+    }
+}
