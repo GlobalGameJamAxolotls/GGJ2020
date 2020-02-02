690bcdca30d8adc7402c2944410b5cb862677f42
JPBarroso <42369917+JPBarroso@users.noreply.github.com>
2020-02-02 16:03:41 +0100

    Scene Management finished

--- GGJ2020Axolotl/Assets/Scripts/SceneMGR.cs
+++ GGJ2020Axolotl/Assets/Scripts/SceneMGR.cs
@@ -2,12 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneMGR : MonoBehaviour
 {
-
+    public GameObject panel;
     public void SceneChanger()
     {
         SceneManager.LoadScene("Tutorial");
+    }
+    public void PanelToggle()
+    {
+        if(panel.activeSelf)
+        {
+            panel.SetActive(false);
+        }
+        else if (!panel.activeSelf)
+        {
+            panel.SetActive(true);
+        }
+    }
+    public void Exit()
+    {
+        Application.Quit();
     }
 }
