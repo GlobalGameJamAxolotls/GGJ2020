1533ec00bd5f476c1feb0f37c4955187b904885d
dialcforchris <mrchrismugridge@gmail.com>
2020-02-02 10:41:48 +0000

    movement unity

--- GGJ2020Axolotl/Assets/Scripts/PlayerCTR.cs
+++ GGJ2020Axolotl/Assets/Scripts/PlayerCTR.cs
@@ -27,6 +27,13 @@ public class PlayerCTR : MonoBehaviour
 
     private bool _pressedSomething;
 
+
+    Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -38,25 +45,25 @@ public class PlayerCTR : MonoBehaviour
         _target = Vector3.zero;
         _pressedSomething = false;
 
-        if (Input.GetKey(InputSystem.Up))
+        if (Input.GetKey(InputSystem.Down))
         {
             _target.x++;
             _pressedSomething = true;
         }
 
-        if (Input.GetKey(InputSystem.Down))
+        if (Input.GetKey(InputSystem.Up))
         {
             _target.x--;
             _pressedSomething = true;
         }
 
-        if (Input.GetKey(InputSystem.Left))
+        if (Input.GetKey(InputSystem.Right))
         {
             _target.z++;
             _pressedSomething = true;
         }
 
-        if (Input.GetKey(InputSystem.Right))
+        if (Input.GetKey(InputSystem.Left))
         {
             _target.z--;
             _pressedSomething = true;
@@ -69,7 +76,7 @@ public class PlayerCTR : MonoBehaviour
 
         if (_pressedSomething)
         {
-            Rotation();
+       //     Rotation();
         }
         Jump();
     }
@@ -77,18 +84,36 @@ public class PlayerCTR : MonoBehaviour
 
     void Movement()
     {
-        transform.Translate(_target * movementSpeed * Time.deltaTime, Space.Self);
-    }
+        float moveHorizontal = Input.GetAxis("Horizontal"); float moveVertical = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(moveHorizontal, 0, moveVertical);
+        anim.SetBool("Walking", move != Vector3.zero);
 
-    private void Rotation()
-    {
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_target), 0.15F);
+        if (moveHorizontal == 0 && moveVertical == 0) return;
 
-        if (_target != Vector3.zero)
+        Vector3 movement = new Vector3(-moveHorizontal, 0f, -moveVertical); transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+
+        if (movement != Vector3.zero)
         {
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_target.normalized), 0.2f);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement.normalized), 0.2f);
         }
+
+
+
+        transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
+
+        //    anim.SetBool("Walking", _target != Vector3.zero);
+        //    transform.Translate(_target * movementSpeed * Time.deltaTime, Space.Self);
     }
+
+    //private void Rotation()
+    //{
+    //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_target), 0.15F);
+
+    //    if (_target != Vector3.zero)
+    //    {
+    //        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_target.normalized), 0.2f);
+    //    }
+    //}
 
     void Jump()
     {
