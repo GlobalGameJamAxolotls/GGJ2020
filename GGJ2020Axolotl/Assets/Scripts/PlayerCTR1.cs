04c68192a6185c3d6d190dddf02dcfc424fe348a
dialcforchris <mrchrismugridge@gmail.com>
2020-02-02 10:07:35 +0000

    YEAH, STRIAGHT TO MAIN SORRY

--- GGJ2020Axolotl/Assets/Scripts/PlayerCTR1.cs
+++ GGJ2020Axolotl/Assets/Scripts/PlayerCTR1.cs
@@ -25,10 +25,7 @@ public class PlayerCTR1 : MonoBehaviour
     {
         Movement();
         Jump();
-        if (!Input.anyKey)
-        {
-            anim.SetBool("Walking", false);
-        }
+      
     }
 
 
@@ -36,20 +33,22 @@ public class PlayerCTR1 : MonoBehaviour
     {
         {
             float moveHorizontal = Input.GetAxis("Horizontal2"); float moveVertical = Input.GetAxis("Vertical2");
+            Vector3 move = new Vector3(moveHorizontal, 0, moveVertical);
+            anim.SetBool("Walking", move != Vector3.zero);
 
             if (moveHorizontal == 0 && moveVertical == 0) return;
 
-            Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical); transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
+            Vector3 movement = new Vector3(-moveHorizontal, 0f, -moveVertical); transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15F);
 
             if (movement != Vector3.zero)
             {
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement.normalized), 0.2f);
             }
-            
 
 
+          
             transform.Translate(movement * movementSpeed * Time.deltaTime, Space.World);
-            anim.SetBool("Walking",true);
+          
 
         }
     }
@@ -57,7 +56,7 @@ public class PlayerCTR1 : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.KeypadEnter) && isjumping == false) 
         {
-            anim.SetBool("Walking", false);
+           // anim.SetBool("Walking", false);
             rb.AddForce(new Vector3(0, jumpForce, 0),ForceMode.Impulse);
             isjumping = true;
         }            
