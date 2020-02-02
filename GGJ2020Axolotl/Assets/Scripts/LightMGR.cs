7c183afce6fbfd4b343e7aca261511a41ac2184a
JPBarroso <42369917+JPBarroso@users.noreply.github.com>
2020-02-02 11:29:23 +0100

    Implemented flickering light, skybox, and fixed a few bugs.
    I want to sleep.

--- GGJ2020Axolotl/Assets/Scripts/LightMGR.cs
+++ GGJ2020Axolotl/Assets/Scripts/LightMGR.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class LightMGR : MonoBehaviour
 {
     private new Light light;
+    [Range(0.9f,1.0f)]
+    public float eventThreshold;
     // Start is called before the first frame update
     void Start()
     {
-        light = GetComponent<Light>();  
+        light = GetComponent<Light>(); 
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Random.value > 0.97) //a random chance
+        if (Random.value > eventThreshold) //UNSTABLE. The slightest change and it goes haywire. Keep it >0.9 <0.99
         {
             if (light.enabled == true) //if the light is on...
             {
